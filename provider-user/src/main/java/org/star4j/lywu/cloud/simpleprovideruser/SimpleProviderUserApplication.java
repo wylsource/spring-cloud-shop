123e8b966dada91c4ce9a2466e9293a6ebd578bc
wuyl <wyl_xlj@163.com>
2017-10-11 18:15:41 +0800

    使用eureka集成微服务

--- provider-user/src/main/java/org/star4j/lywu/cloud/simpleprovideruser/SimpleProviderUserApplication.java
+++ provider-user/src/main/java/org/star4j/lywu/cloud/simpleprovideruser/SimpleProviderUserApplication.java
@@ -2,8 +2,10 @@ package org.star4j.lywu.cloud.simpleprovideruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SimpleProviderUserApplication {
 
 	public static void main(String[] args) {
--- provider-user/src/main/java/org/star4j/lywu/cloud/simpleprovideruser/controller/UserController.java
+++ provider-user/src/main/java/org/star4j/lywu/cloud/simpleprovideruser/controller/UserController.java
@@ -1,11 +1,20 @@
 package org.star4j.lywu.cloud.simpleprovideruser.controller;
 
+import static org.mockito.Matchers.longThat;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.star4j.lywu.cloud.simpleprovideruser.entity.User;
 import org.star4j.lywu.cloud.simpleprovideruser.repository.UserRepository;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 
 /**
  * @Author: WuYL
@@ -18,9 +27,28 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    
+    @Autowired
+    private EurekaClient eurekaClient;
+    
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/simple/{id}")
     public User findById(@PathVariable Long id){
         return userRepository.findOne(id);
+    }
+    
+    @GetMapping("/service/url")
+    public String serviceUrl() {
+        InstanceInfo instance = eurekaClient.getNextServerFromEureka("PROVIDER-USER", false);
+        return instance.getHomePageUrl();
+    }
+    
+    @GetMapping("/service/info")
+    public String serviceInfo() {
+    	List<String> localServiceInstance = discoveryClient.getServices();
+    	System.out.println("------"+localServiceInstance.toString());
+    	return localServiceInstance.get(0);
     }
 }
